5f925b18941ef13d79bb1e1218d2f9195f2f56dc
BetanzosSotoJorgeAlonso <jbetanzoss2300@alumno.ipn.mx>
2025-10-13 19:24:10 -0600

    Resuelto el 2

--- 06_Algoritmia/js/algoritmia.js
+++ 06_Algoritmia/js/algoritmia.js
@@ -4,6 +4,7 @@ function problema1(){
         document.getElementById("p1-output").textContent = "Por favor, ingresa texto.";
         return;
     }
+    
     const palabras = input.split(" ");
     const invertidas = palabras.reverse().join(" ");
     document.getElementById("p1-output").textContent = invertidas;
@@ -11,6 +12,32 @@ function problema1(){
 
 function problema2(){
 
+    var p2_x1 = document.querySelector("#p2_x1"),value;
+    var p2_x2 = document.querySelector("#p2_x2"),value;
+    var p2_x3 = document.querySelector("#p2_x3"),value;
+    var p2_x4 = document.querySelector("#p2_x4"),value;
+    var p2_x5 = document.querySelector("#p2_x5"),value;
+
+    var p2_y1 = document.querySelector("#p2_y1"),value;
+    var p2_y2 = document.querySelector("#p2_y2"),value;
+    var p2_y3 = document.querySelector("#p2_y3"),value;
+    var p2_y4 = document.querySelector("#p2_y4"),value;
+    var p2_y5 = document.querySelector("#p2_y5"),value;
+
+    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+    v1 = v1.sort(function(a, b){return b-a});
+    v2 = v2.sort(function(a, b){return b-a});
+
+    v2 = v2.reverse();
+
+    var p2_producto = 0;
+    for(var i=0; i<v1.length; i++){
+        p2_producto += v1[i] * v2[i];
+    }
+    document.querySelector("#p2-resultado").textContent = "El producto escalar mínimo es: " + p2_producto;
+    
 }
 
 function problema3(){
@@ -20,8 +47,8 @@ function problema3(){
         return;
     }
     const palabras = input.split(",");
-    let maxPalabra = "";
-    let maxUnicos = 0;
+    var maxPalabra = "";
+    var maxUnicos = 0;
 
     for (let palabra of palabras) {
         palabra = palabra.toUpperCase().replace(/[^A-Z]/g, "");
